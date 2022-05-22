3b07f957f6cb9dc629860eea8250b8f72ceb99b1
esa <esapci@gmail.com>
2022-05-22 23:03:39 +0800

    update

--- app.py
+++ app.py
@@ -101,16 +101,12 @@ def chk_primary_keys (df, list_of_keys):
     if len (dup_dict) > 0:
         # print the error 
         
-        st.header  ('There are  Primary Key Conflicts. Please check the files and correct the  primary key conflicts.' )
-        #st.subheader ('The columns that have the  primary key conficts are named:')
-        
-        #for keys in list_of_keys:
-        #    st.write (keys)
-        #st.write  (list_of_keys)
-        st.header  ('The details of the duplicates are :')
-        st.write ('(Format of output below is  primary key 1 : primary key 2)')
-        #st.write (dup_dict)
-        # {2: [15], 6599: [15], 6603: [15]}
+        st.header  ('There are  Primary Key Conflicts.' )
+   
+        st.subheader  ('The details of the duplicates/conficts are :')
+       
+        str_1 = str(list_of_keys[0]) + ' : ' + str(list_of_keys[1]) 
+        st.write ('Format of output is : ' + str_1 )
         
         for key in dup_dict:
             string = str(key) + ' : ' + str(dup_dict[key])
@@ -127,72 +123,22 @@ def chk_primary_keys (df, list_of_keys):
 
 
 
-
     
-def process(uploaded_files):
     
-    # check if uploaded file is a csv file
-    #input_csv = is_csv (uploaded_files)
-    
-    input_csv = True
-    
-    if input_csv :
-    
-        df = pd.read_csv (uploaded_files)
-        # rename 2 columns
-        # make sure all lowcaps
-        
-        
-        list_of_keys  = ['key_1','key_2']
-        dict = chk_primary_keys (df, list_of_keys)
-        
-
-        #st.write(df)
-    else :
-        st.write ('Not a csv file')
-        
-     
-    
-def process_1(df, list_of_keys):
+def process(df, list_of_keys):
     
     # check if uploaded file is a csv file
     #input_csv = is_csv (uploaded_files)
     
     #list_of_keys  = ['key_1','key_2']
-    with st.spinner('Wait for it...'):
+    with st.spinner('Procesing....'):
         
         chk_primary_keys (df, list_of_keys)
         
 
 
             
-def main_1 ():
- 
-    st.markdown(""" <style> #MainMenu {visibility: hidden;} 
-                footer {visibility: hidden;}</style> """, 
-                unsafe_allow_html=True)
-    
-    with st.sidebar:
-    
-        #date = st.date_input ('Select Date Of Incoming')
-        #code = st.text_input ('Enter Rm Code')
-        #qty = st.text_input ('Enter Qty in kgs' )
-        
-        
-        uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False, type='csv')
-        
-        
-        '''
-        spectra = st.file_uploader("upload file", type={"csv", "txt"})
-        if spectra is not None:
-            spectra_df = pd.read_csv(spectra)
-        st.write(spectra_df)
-        '''
 
-        
-        st.button ('Press Enter', key='enter',on_click= process , args = [uploaded_files])
-
-    
             
 def main ():
     
@@ -205,37 +151,20 @@ def main ():
     
     st.header ('FILE REQUIREMENTS')
     st.subheader ('Pls read carefully before using this app')
-    st.write ('1) Must be a csv file')
-    st.write ('2) Must have exactly 2 columns w/c signifies the primary keys ')
-    st.write ('3) Values much have no white space and no null values ')
-    st.write ('4) Each Column must have the same number of rows')
-    st.write ('5) Format of row values in each column must be of the same format  ')
-    st.write ('6) Impt to note that the ff are NOT the same : ')
+    
     
     '''
-        'a) W-2 , w2 , w-2  = must be the same asccii value')
-        'b) 12  3 , 123 , 1  23  ')
-        'c) 123 , 123.00  = must either both be an integer or both be a float ')
+
+    * (1)  Must be a csv file
+    * (2)  Must have exactly 2 columns w/c signifies the primary keys 
+    * (3)  Values much have no white space and no null values 
+    * (4)  Each Column must have the same number of rows
+    * (5)  Format of row values in each column must be of the same format 
+    * (6)  Impt to note that the ff are NOT the same : 
+        *   W-2 , w2 , w-2  = as they have different asccii value
+        *   12  3 , 123 , 1  23  
+        *   123 , 123.00  =  must either both be an integer or both be a float
     '''
-    
-
-
-    
-    col1, col2,col3,col4 = st.columns(4)
-    
-    with col1 :
-        pass
-    with col2:
-        st.write ( 'a) W-2 , w2 , w-2  = must be the same asccii value')
-        st.write ( 'b) 12  3 , 123 , 1  23  ')
-        st.write ( 'c) 123 , 123.00  = must either both be an integer or both be a float ')
-    with col3:
-        pass
-    with col4:
-        pass
-        
-    st.empty ()
- 
     
     
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False, type={'csv'} )
@@ -249,12 +178,8 @@ def main ():
             # col  should be 2 columns only
             return
         else:
-            process_1 (df, old_col_names)
+            process (df, old_col_names)
     
-
-    
-    #st.button ('Press Enter', key='enter',on_click= process , args = [uploaded_files])
-
     
     
     
